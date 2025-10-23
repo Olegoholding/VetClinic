e1898eaad2886812dc07144624f2a03abb0a2263
KNKNT <kizilov.kiza573@gmail.com>
2025-10-23 23:39:39 +0300

    Добавьте файлы проекта.

---
+++ DataPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VetClinic
+{
+    /// <summary>
+    /// Логика взаимодействия для DataPage.xaml
+    /// </summary>
+    public partial class DataPage : Page
+    {
+        private string _name;
+        public DataPage(string name)
+        {
+            InitializeComponent();
+
+            _name = name;
+            DirName.Text = _name;
+        }
+
+        private void ToMainWindow_MouseDown(object sender, MouseButtonEventArgs e) => ((MainWindow)Application.Current.MainWindow).Frame.Content = null;
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VetClinic
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) => Frame.Navigate(new DataPage("Вкладка"));
+        
+
+        private void ThemeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) =>
+           Application.Current.Resources["Current"] = (ThemeComboBox.SelectedIndex == 0) ?
+           (SolidColorBrush)Application.Current.Resources["LightTheme"] :
+           (SolidColorBrush)Application.Current.Resources["DarkTheme"];
+
+    }
+}
